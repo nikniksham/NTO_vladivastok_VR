ffe4ae2c5764c48e219d6f4a8d27ebcc7792af6b
Rjkzavr <kolya.toropof@gmail.com>
2022-01-07 15:46:31 +0300

    tests

--- auto_test.py
+++ auto_test.py
@@ -1,27 +1,15 @@
 from Solution_first_problem import calculate
 
 tests = [
-    ['Tinput1.txt', '1'],
-    ['Tinput2.txt', '0'],
-    ['Tinput1.txt', '1'],
-    ['Tinput1.txt', '1'],
-    ['Tinput1.txt', '1'],
-    ['Tinput2.txt', '0'],
-    ['Tinput1.txt', '1'],
-    ['Tinput1.txt', '1'],
-    ['Tinput1.txt', '1'],
+    ['input.txt', '1 -0.15892 1.89097'],
 ]
-
-
-def calculate(t):
-    return '1'
-
 
 is_only_wrong = True
 
 for i, test in enumerate(tests):
     file, result = test
-    if calculate(file) == result and not is_only_wrong:
-        print(f'test №{i+1} OK')
-    elif calculate(file) != result:
-        print(f'\ntest №{i+1} WA\nAnswer: {calculate(file)}\nTrue:   {result}\n')
+    with open(file, 'r') as f:
+        if calculate(f) == result and not is_only_wrong:
+            print(f'test №{i+1} OK')
+        elif calculate(f) != result:
+            print(f'\ntest №{i+1} WA\nAnswer: {calculate(f)}\nTrue:   {result}\n')
