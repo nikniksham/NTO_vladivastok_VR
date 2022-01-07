d8e365d9be6b66cf6cc82f7adf52f2b4b5e8c990
Rjkzavr <kolya.toropof@gmail.com>
2022-01-07 13:44:05 +0300

    add test generator

---
+++ obj_test_generator.py
@@ -0,0 +1,16 @@
+with open('input6.obj') as r:
+    vertexes = []
+    edges = []
+    from_zero = True
+    for line in r.readlines():
+        if line.startswith('v '):
+            vertexes.append(tuple(map(float, line[2:].split())))
+        if line.startswith('f '):
+            edges.append(tuple(map(lambda x: int(x.split('/')[0]) - (1 if from_zero else 0), line[2:].split())))
+    print(len(vertexes))
+    for vertex in vertexes:
+        print(*vertex)
+    print()
+    print(len(edges))
+    for edge in edges:
+        print(len(edge), *edge)
